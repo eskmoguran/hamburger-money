14c1300f69abd124572aae2191eb2350ce06fd59
eskmoguran <mtshibaura112577@yahoo.co.jp>
2019-04-17 23:00:19 +0900

    不要なコードとファイルを削除

--- hamburger-money-project/src/views/Home.vue
+++ hamburger-money-project/src/views/Home.vue
@@ -1,13 +1,7 @@
 <template>
-  <HelloWorld />
+  <div>test</div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    components: {
-      HelloWorld
-    }
-  }
+export default {};
 </script>
